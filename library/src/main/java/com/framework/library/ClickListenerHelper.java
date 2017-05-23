1ec77e822d7ef87fc63c7d1504b834f756fde37f
sNajdan <snajdan0525@gmail.com>
2017-05-23 23:31:31 +0800

    获取View上的onClickListener对象，并且适配API14以上和API14以下两种情况

--- library/src/main/java/com/framework/library/ClickListenerHelper.java
+++ library/src/main/java/com/framework/library/ClickListenerHelper.java
@@ -1,10 +1,60 @@
 package com.framework.library;
 
+import android.view.View;
+
+import java.lang.reflect.Field;
+
 /**
  * 用于获取当前View设置的onClickerListener
  * Created by snajdan on 2017/5/23.
  */
 
 public class ClickListenerHelper {
+    //Used for APIs lower than ICS (API 14)
+    private static View.OnClickListener getOnClickListenerV(View view) {
+        View.OnClickListener retrievedListener = null;
+        String viewStr = "android.view.View";
+        Field field;
 
+        try {
+            field = Class.forName(viewStr).getDeclaredField("mOnClickListener");
+            retrievedListener = (View.OnClickListener) field.get(view);
+        } catch (NoSuchFieldException ex) {
+        } catch (IllegalAccessException ex) {
+        } catch (ClassNotFoundException ex) {
+        } catch (Throwable t) {
+
+        }
+
+        return retrievedListener;
+    }
+
+    //Used for new ListenerInfo class structure used beginning with API 14 (ICS)
+    private static View.OnClickListener getOnClickListenerV14(View view) {
+        View.OnClickListener retrievedListener = null;
+        String viewStr = "android.view.View";
+        String lInfoStr = "android.view.View$ListenerInfo";
+
+        try {
+            Field listenerField = Class.forName(viewStr).getDeclaredField("mListenerInfo");
+            Object listenerInfo = null;
+
+            if (listenerField != null) {
+                listenerField.setAccessible(true);
+                listenerInfo = listenerField.get(view);
+            }
+
+            Field clickListenerField = Class.forName(lInfoStr).getDeclaredField("mOnClickListener");
+
+            if (clickListenerField != null && listenerInfo != null) {
+                retrievedListener = (View.OnClickListener) clickListenerField.get(listenerInfo);
+            }
+        } catch (NoSuchFieldException ex) {
+        } catch (IllegalAccessException ex) {
+        } catch (ClassNotFoundException ex) {
+        } catch (Throwable t) {
+
+        }
+        return retrievedListener;
+    }
 }
